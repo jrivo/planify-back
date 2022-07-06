91805b6288ab7704a1a484a7c9076eac4fd10125
Jonathan Rivo <rivo.jonathan98@gmail.com>
2022-07-06 16:08:01 +0200

    Added routes for swagger

--- swagger.js
+++ swagger.js
@@ -1,6 +1,6 @@
 const swaggerAutogen = require('swagger-autogen')()
 
 const outputFile = './swagger_output.json'
-const endpointsFiles = ['./app/routes/auth.routes','./app/routes/user.routes','./app/routes/place.routes']
+const endpointsFiles = ['./app/routes/auth.routes','./app/routes/user.routes','./app/routes/place.routes','./app/routes/trip.routes','./app/routes/placeType.routes','./app/routes/activity.routes']
 
 swaggerAutogen(outputFile, endpointsFiles)
