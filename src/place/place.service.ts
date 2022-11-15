f173a5cfc9f6cc4e33bc30bc33600e66a9c7e5be
Jonathan Rivo <rivo.jonathan98@gmail.com>
2022-11-15 22:33:00 +0100

    docker restart always policy

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -24,6 +24,7 @@ export class PlaceService {
       include: {
         activities: {
           include: {
+            //TODO: add address to activity
             medias: true,
           },
         },
