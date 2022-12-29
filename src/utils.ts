dddfbdd140b08255bc77bbefc83519121599a69e
Jonathan Rivo <jonathanrivo@mbp-de-jonathan.home>
2022-12-29 15:41:22 +0100

    Debugging url in mails

--- src/utils.ts
+++ src/utils.ts
@@ -96,6 +96,8 @@ export const sendVerificationEmail = function (to, token) {
       verification_link: `http://${APP_URL}/email-verification?token=${token}`,
     },
   };
+  console.log("MESSAGE")
+  console.log(`http://${APP_URL}/email-verification?token=${token}`)
   sgMail
     .send(message)
     .then((res) => {
