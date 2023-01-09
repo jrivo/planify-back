5e7da242dc2f73644481ea064461bb8be7bc7d3d
Jonathan Rivo <jonathanrivo@mbp-de-jonathan.home>
2023-01-09 23:55:35 +0100

    Fixed root test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello Planify API!"', () => {
+      expect(appController.getHello()).toBe('Hello Planify API!');
     });
   });
 });
